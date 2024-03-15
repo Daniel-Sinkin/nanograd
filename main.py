30bdf9a1c0454f1bde17d69d6989e1c6489d9a84
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-03-15 22:40:39 +0100

    Added main with example code that call the visualize function

---
+++ main.py
@@ -0,0 +1,29 @@
+"""Main file for the NanoGrad package."""
+
+from src.autograd import NanoTensor
+from src.autolabel_tensors import auto_label
+
+
+def main():
+    """Define some simple computation graph and visualize it."""
+    a = NanoTensor(2.0, label="a")
+    b = NanoTensor(3.0, label="b")
+    c = NanoTensor(4.0, label="c")
+
+    d = a + b
+    e = b * c
+    f = c.sin()
+    g = f + a
+    h = d + e
+    i = h * g
+    j = g * i
+    j.backward()
+
+    for tensor in [d, e, f, g, h, i, j]:
+        auto_label(tensor)
+
+    j.visualize_graph()
+
+
+if __name__ == "__main__":
+    main()
