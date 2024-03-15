b96791b9439bcc99fa843390fe8c665fe0f17a6a
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-03-15 13:11:17 +0100

    Added some more autogra experimentation

--- src/cgnode.py
+++ src/cgnode.py
@@ -104,9 +104,9 @@ if __name__ == "__main__":
     _n1 = CGNodeLeaf(0.7)
     _n2 = CGNodeUnary(UnaryOperator.INV, _n1)
     _n3 = CGNodeBinary(BinaryOperator.ADD, _n1, _n2)
-    _n4 = CGNodeBinary(BinaryOperator.MUL, _n1, _n2)
+    _n4 = CGNodeBinary(BinaryOperator.MUL, _n3, _n2)
 
     assert _n1.eval_() == 0.7
     assert _n2.eval_() == 1 / 0.7
     assert _n3.eval_() == 0.7 + 1 / 0.7
-    assert _n4.eval_() == 0.7 * 1 / 0.7
+    assert _n4.eval_() == (0.7 + 1 / 0.7) * (1 / 0.7)
