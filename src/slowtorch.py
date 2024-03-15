f92db10a0e73bf6f395ca1c0d9c3d6468392f56e
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-03-16 00:55:34 +0100

    Minor adjustments to function signature

--- src/slowtorch.py
+++ src/slowtorch.py
@@ -14,9 +14,9 @@ class Neuron:
     def __init__(self, n_args: int = 2, seed=RNG_SEED):
         _rng = np.random.default_rng(seed)
         self.w: list[NanoTensor] = [
-            NanoTensor(float(x)) for x in _rng.normal(1, 0.01, n_args)
+            NanoTensor(float(x)) for x in _rng.uniform(-1.0, 1.0, n_args)
         ]
-        self.b = NanoTensor(float(_rng.normal(1, 0.01, 1)[0]))
+        self.b = NanoTensor(float(_rng.uniform(-1.0, 1.0)))
 
     def __repr__(self):
         return f"Neuron({self.w}, {self.b})"
@@ -35,8 +35,9 @@ class Layer:
     def __iter__(self) -> Iterator[Neuron]:
         return iter(self._neurons)
 
-    def __call__(self, x: NanoTensor) -> list[NanoTensor]:
-        return [n(x) for n in self]
+    def __call__(self, x: NanoTensor) -> NanoTensor | list[NanoTensor]:
+        retval: list[NanoTensor] = [n(x) for n in self]
+        return retval[0] if len(retval) == 1 else retval
 
 
 class MLP:
@@ -44,10 +45,10 @@ class MLP:
         sz = [nin] + nouts
         self._layers = [Layer(sz[i], sz[i + 1]) for i in range(len(nouts))]
 
-    def __call__(self, x: NanoTensor) -> list[NanoTensor]:
+    def __call__(self, x: NanoTensor) -> NanoTensor | list[NanoTensor]:
         return self.forward(x)
 
-    def forward(self, x: NanoTensor) -> list[NanoTensor]:
+    def forward(self, x: NanoTensor) -> NanoTensor | list[NanoTensor]:
         for layer in self._layers:
-            x: list[NanoTensor] = layer(x)
+            x: NanoTensor | list[NanoTensor] = layer(x)
         return x
